1a3b0ab26a1b6ce93137152ffd01a65d731be9c8
Russel Pickering <75551096+SkellyVamps@users.noreply.github.com>
2026-09-06 19:19:05 -0400

    Move HOTAS controls into the native Settings menu
    Add a native HOTAS tab to Arma Reforger Settings, select custom HOTAS configs from the profile folder through the game's keybind module, remove the F10 standalone menu/action/configs, and stop force-loading the Workbench test config.

---
+++ Scripts/Game/HOTASDebugger/HOTASSettingsTab.c
@@ -0,0 +1,234 @@
+class HOTASSettingsSubMenu : SCR_SettingsSubMenuBase
+{
+	protected SCR_SpinBoxComponent m_HotasConfig;
+	protected ref array<SCR_SpinBoxComponent> m_HudControls = {};
+	protected ref array<string> m_UserConfigs = {};
+	protected bool m_bLoading;
+
+	//------------------------------------------------------------------------------------------------
+	override void OnTabCreate(Widget menuRoot, ResourceName buttonsLayout, int index)
+	{
+		super.OnTabCreate(menuRoot, buttonsLayout, index);
+
+		m_bLoading = true;
+		HOTASDebugController.GetInstance().ReloadHudSettings();
+
+		m_HotasConfig = FindSpinBox("HOTASConfig");
+		SetupHotasConfigSelector();
+		SetupHudControls();
+		m_bLoading = false;
+	}
+
+	//------------------------------------------------------------------------------------------------
+	override void OnTabShow()
+	{
+		super.OnTabShow();
+
+		m_bLoading = true;
+		HOTASDebugController.GetInstance().ReloadHudSettings();
+		SyncHotasConfigSelector();
+		SyncHudControls();
+		m_bLoading = false;
+	}
+
+	//------------------------------------------------------------------------------------------------
+	protected SCR_SpinBoxComponent FindSpinBox(string widgetName)
+	{
+		Widget widget = m_wRoot.FindAnyWidget(widgetName);
+		if (!widget)
+			return null;
+
+		return SCR_SpinBoxComponent.Cast(widget.FindHandler(SCR_SpinBoxComponent));
+	}
+
+	//------------------------------------------------------------------------------------------------
+	protected SCR_SettingsManagerKeybindModule GetKeybindModule()
+	{
+		return SCR_SettingsManagerKeybindModule.Cast(
+			GetGame().GetSettingsManager().GetModule(ESettingManagerModuleType.SETTINGS_MANAGER_KEYBINDING)
+		);
+	}
+
+	//------------------------------------------------------------------------------------------------
+	protected void SetupHotasConfigSelector()
+	{
+		if (!m_HotasConfig)
+			return;
+
+		m_HotasConfig.ClearAll();
+		m_UserConfigs.Clear();
+
+		m_HotasConfig.AddItem("Default / Other");
+
+		// Match the vanilla Controller Presets settings page: user joystick configs are
+		// discovered directly from the profile customInputConfigs folder.
+		FileIO.FindFiles(m_UserConfigs.Insert, "$profile:.save/settings/customInputConfigs", ".conf");
+		foreach (string config : m_UserConfigs)
+			m_HotasConfig.AddItem(FilePath.StripPath(config));
+
+		SyncHotasConfigSelector();
+		m_HotasConfig.m_OnChanged.Insert(OnHotasConfigChanged);
+	}
+
+	//------------------------------------------------------------------------------------------------
+	protected void SyncHotasConfigSelector()
+	{
+		if (!m_HotasConfig)
+			return;
+
+		SCR_SettingsManagerKeybindModule keybindModule = GetKeybindModule();
+		if (!keybindModule)
+		{
+			m_HotasConfig.SetCurrentItem(0, false, false, false);
+			return;
+		}
+
+		InputBinding binding = keybindModule.GetInputBindings();
+		if (!binding)
+		{
+			m_HotasConfig.SetCurrentItem(0, false, false, false);
+			return;
+		}
+
+		array<ResourceName> activeConfigs = {};
+		binding.GetCustomConfigs(activeConfigs);
+
+		int selected = 0;
+		foreach (int i, string config : m_UserConfigs)
+		{
+			foreach (ResourceName activeConfig : activeConfigs)
+			{
+				if (activeConfig == config)
+				{
+					selected = i + 1;
+					break;
+				}
+			}
+			if (selected > 0)
+				break;
+		}
+
+		m_HotasConfig.SetCurrentItem(selected, false, false, false);
+	}
+
+	//------------------------------------------------------------------------------------------------
+	protected void OnHotasConfigChanged(SCR_SpinBoxComponent component, int index)
+	{
+		if (m_bLoading)
+			return;
+
+		SCR_SettingsManagerKeybindModule keybindModule = GetKeybindModule();
+		if (!keybindModule)
+			return;
+
+		InputBinding binding = keybindModule.GetInputBindings();
+		if (!binding)
+			return;
+
+		if (index <= 0)
+		{
+			array<ResourceName> emptyConfigs = {};
+			binding.SetCustomConfigs(emptyConfigs);
+			binding.Save();
+			Print("[HOTAS Debugger] Custom HOTAS input config cleared from Settings", LogLevel.NORMAL);
+			return;
+		}
+
+		int configIndex = index - 1;
+		if (!m_UserConfigs.IsIndexValid(configIndex))
+			return;
+
+		string selectedConfig = m_UserConfigs.Get(configIndex);
+		keybindModule.SelectJoystickPresetPath(selectedConfig);
+		Print(string.Format("[HOTAS Debugger] HOTAS input config selected from Settings: %1", selectedConfig), LogLevel.NORMAL);
+	}
+
+	//------------------------------------------------------------------------------------------------
+	protected void SetupHudControls()
+	{
+		m_HudControls.Clear();
+
+		array<string> widgetNames = {
+			"HUDEnabled",
+			"HUDPosition",
+			"HUDScale",
+			"FadeDelay",
+			"FadeDuration",
+			"BackgroundEnabled",
+			"BackgroundOpacity",
+			"RollAxis",
+			"PitchAxis",
+			"ThrottleAxis",
+			"YawAxis",
+			"DebugMode"
+		};
+
+		HOTASDebugController controller = HOTASDebugController.GetInstance();
+		for (int i = 0; i < controller.GetSettingsCount(); i++)
+		{
+			SCR_SpinBoxComponent control = FindSpinBox(widgetNames[i]);
+			m_HudControls.Insert(control);
+			if (!control)
+				continue;
+
+			control.ClearAll();
+			int optionCount = controller.GetSettingOptionCount(i);
+			for (int optionIndex = 0; optionIndex < optionCount; optionIndex++)
+				control.AddItem(controller.GetSettingOptionLabel(i, optionIndex), optionIndex == optionCount - 1);
+
+			control.SetCurrentItem(controller.GetSettingOptionIndex(i), false, false, false);
+			control.m_OnChanged.Insert(OnHudSettingChanged);
+		}
+	}
+
+	//------------------------------------------------------------------------------------------------
+	protected void SyncHudControls()
+	{
+		HOTASDebugController controller = HOTASDebugController.GetInstance();
+		for (int i = 0; i < m_HudControls.Count(); i++)
+		{
+			SCR_SpinBoxComponent control = m_HudControls[i];
+			if (control)
+				control.SetCurrentItem(controller.GetSettingOptionIndex(i), false, false, false);
+		}
+	}
+
+	//------------------------------------------------------------------------------------------------
+	protected void OnHudSettingChanged(SCR_SpinBoxComponent component, int optionIndex)
+	{
+		if (m_bLoading)
+			return;
+
+		for (int i = 0; i < m_HudControls.Count(); i++)
+		{
+			if (m_HudControls[i] != component)
+				continue;
+
+			HOTASDebugController.GetInstance().SetSettingOptionIndex(i, optionIndex);
+			return;
+		}
+	}
+}
+
+//------------------------------------------------------------------------------------------------
+// Add HOTAS as a normal peer of the game's Video / Audio / Interface settings tabs.
+modded class SCR_SettingsSuperMenu
+{
+	protected static const string HOTAS_SETTINGS_TAB_IDENTIFIER = "HOTASInputHUDSettings";
+
+	override void OnMenuOpen()
+	{
+		super.OnMenuOpen();
+
+		if (!m_SuperMenuComponent || !m_SuperMenuComponent.GetTabView())
+			return;
+
+		m_SuperMenuComponent.GetTabView().RemoveTabByIdentifier(HOTAS_SETTINGS_TAB_IDENTIFIER);
+		m_SuperMenuComponent.GetTabView().AddTab(
+			"{8C52D9F7A31B640E}UI/layouts/Menus/SettingsSubMenus/HOTASSettings.layout",
+			"HOTAS",
+			true,
+			identifier: HOTAS_SETTINGS_TAB_IDENTIFIER
+		);
+	}
+}
